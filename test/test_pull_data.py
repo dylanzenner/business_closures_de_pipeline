7ea7559b26be49dce063af95dc8151c1442e5212
Dylan Zenner <zennerdylan23@gmail.com>
2020-12-20 12:37:24 -0600

    upload test_pull_data.py

---
+++ test/test_pull_data.py
@@ -0,0 +1,38 @@
+import pytest
+from pytest import ExitCode
+import mongomock
+import pymongo
+import certifi
+import urllib3
+from Function import pull_socrata, check_connection
+
+
+client = mongomock.MongoClient()
+db = client.test_database
+col = db.test_collection
+
+url = "https://data.sfgov.org/"
+
+http = urllib3.PoolManager(ca_certs=certifi.where())
+http.request("GET", url, retries=False)
+
+
+def test_check_connection():
+    """
+    Tests whether or not check_connection returns either 'Successful Connection'
+    or 'Connection Failed'
+    """
+
+    assert check_connection() in ["Successful Connection", "Connection Failed"]
+
+
+def test_pull_socrata():
+    """
+    Tests that pull_socrata() inserts the data pulled from the API and returns an
+    error only if the error code is not 11000
+    :return:
+    """
+    if check_connection() == "Successful Connection":
+        pull_socrata()
+
+    assert col != []
